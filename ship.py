e669c5f23677512e59ea7a36528adefc6079d0b5
jordanmjacobson <33322072+jordanmjacobson@users.noreply.github.com>
2017-11-19 17:28:57 -0500

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,22 +1,10 @@
 import pygame
 class ship:
-    def __init__(self, image: str):
-        self.image = pygame.Surface((50, 50))
-        self.rect = self.image.get_rect()
-        #angle of gun
+    def __init__(self, image: str, xcoor, ycoor):
+        self.image = pygame.image.load("Path")
+        self.x = xcoor
+        self.y = ycoor
     def moveRight(self):
-        self.rect.x += 1
-class bullet:
-    def __init(self,speed):
-        self.speed = speed
-class aliens:
-    def __init__(self,speed):
-        self.speed = speed
-    def moveDown(self):
-class controller:
-    def __init__(self):
-        self.speedUp
-class score:
-    def __init__(self):
-        self.accum = 0
-        
+        self.x += 1
+    def moveLeft(self):
+        self.x -= 1
