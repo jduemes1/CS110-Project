69fbe29bcc2d0d9dfee7e0a107d10a9292e5cd5e
jordanmjacobson <33322072+jordanmjacobson@users.noreply.github.com>
2017-11-27 18:37:59 -0500

    Update score.py

--- score.py
+++ score.py
@@ -1,9 +1,8 @@
 class score:
     def __init__(self):
         self.count = 0
-    def increase(self, k = False):
-        if k:
-            self.count+=10
-        else:
-            self.count+=1
-        return self.count
+    def increase(self):
+        self.count+=100
+        
+    
+ 
