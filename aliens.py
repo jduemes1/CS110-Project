07f03fc5e4e79c0f887899abc31969734a67e947
jordanmjacobson <33322072+jordanmjacobson@users.noreply.github.com>
2017-11-27 18:23:14 -0500

    Update aliens.py

--- aliens.py
+++ aliens.py
@@ -3,14 +3,14 @@ import random
 class aliens(pygame.sprite.Sprite):
     def __init__(self, pictures):
         pygame.sprite.Sprite.__init__(self)
-        self.x = random.randrange(1001)
+        self.x = random.randrange(931)
         self.y = 0
         self.pict = random.choice(pictures)
         self.image = pygame.image.load(self.pict)
-        self.image = pygame.transform.scale(self.image, (65,65))
+        self.image = pygame.transform.scale(self.image, (100,100))
         self.rect = self.image.get_rect(topleft = (self.x,self.y))
-    def move(self, num =1):
-        self.rect.y+=num
+    def move(self, speed):
+        self.rect.y+=speed
     def exit(self):
         self.rect.x = 1000
         self.rect.y = 700
