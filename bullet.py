f85dd44a4f0fb8437448d4d282421eb420cc7984
jordanmjacobson <33322072+jordanmjacobson@users.noreply.github.com>
2017-11-27 18:34:58 -0500

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,12 +1,14 @@
 import pygame
 class bullet(pygame.sprite.Sprite):
-    def __init__(self,speed):
-        self.speed = speed
-        self.image = pygame.surface((5,10))
+    def __init__(self,xcoor,ycoor):
+        self.image = pygame.surface((6,20))
+        self.x = xcoor
+        self.y = ycoor
+        self.rect = self.image.get_rect(center = (self.x,self.y))
+        
     def shootBullet(self):
-        self.y=0
-        for i in range("""height of screen"""):
-            self.y+=1
-            if """collosion with alien""":
-                """destroy alien"""
-                """destroy itself"""
+        self.rect.y-=5
+    def exit(self)
+        self.rect.x = 1000
+       
+        
