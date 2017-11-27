6e08f18d166d9db398f5ecb9af5ed7e28bad440b
dlam15 <31551025+dlam15@users.noreply.github.com>
2017-11-26 21:29:44 -0500

    play button

---
+++ Start.py
@@ -0,0 +1,55 @@
+import pygame
+
+WIDTH = 1000
+HEIGHT = 700
+
+class Start:
+    def __init__(self):
+        pygame.init()
+        self.display = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.background = pygame.Surface(self.display.get_size()).convert()
+        self.font = pygame.font.SysFont('comicsansms',30)
+        self.fonty = pygame.font.SysFont('lucidaconsole',70)
+        self.play = self.font.render('Play',True,(0,255,0))
+        self.display.blit(self.play,(400,500))
+        self.room = pygame.image.load('classroom.jpg')
+        self.room = pygame.transform.scale(self.room,(1000,700))
+        self.display.blit(self.room,(0,0))
+        pygame.display.flip()
+        self.mouse = pygame.mouse.get_pos()
+        letter = 'Comp Sci Invaders'
+        self.x = 150
+        for c in letter:
+            self.text = self.fonty.render(c,True,(0,0,255))
+            pygame.time.delay(50)
+            self.display.blit(self.text,(self.x,200))
+            self.background.blit(self.display,(self.x,350))
+            self.x += 40
+            pygame.display.flip()
+
+        
+    def choice(self):
+        done = False
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+                elif event.type == pygame.MOUSEMOTION:
+                    self.mouse = pygame.mouse.get_pos()
+
+                    #print(self.mouse)
+                    if 400<self.mouse[0]<470 and 500<self.mouse[1]<540:
+                        pygame.draw.rect(self.display, (255,255,255),(400,500,70,45))
+                        self.display.blit(self.play,(400,500))
+                        pygame.display.flip()
+
+                    else:
+                        pygame.draw.rect(self.display, (0,0,0),(400,500,70,45))
+                        self.display.blit(self.play,(400,500))
+                        pygame.display.flip()
+                                                
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if pygame.mouse.get_pressed()[0] and 400<self.mouse[0]<470 and 500<self.mouse[1]<550:
+                        return True
+            pygame.display.flip()
+        pygame.quit()
