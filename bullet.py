422da575949ed69b1a7ce83e9bca9527755778d6
jordanmjacobson <33322072+jordanmjacobson@users.noreply.github.com>
2017-11-27 19:14:59 -0500

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -3,6 +3,7 @@ class bullet(pygame.sprite.Sprite):
     def __init__(self,xcoor,ycoor):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((6,20))
+        self.image.fill((255,0,0))
         self.x = xcoor
         self.y = ycoor
         self.rect = self.image.get_rect(center = (self.x,self.y))
