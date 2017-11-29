10ed9a804a7de4ca44bece79327fa7558ebaea64
jordanmjacobson <33322072+jordanmjacobson@users.noreply.github.com>
2017-11-28 19:20:56 -0500

    Update score.py

--- score.py
+++ score.py
@@ -2,6 +2,11 @@ class score:
     def __init__(self):
         self.count = 0
     def increase(self):
+        """
+        Increases the score 
+        params: none
+        returns: none
+        """
         self.count+=100
         
     
